475fb10766e8b61dbc2d6f33db36be721011b31b
ramLandmark <ramananda@stylishop.com>
2025-08-09 01:21:17 +0530

    Implement useRef example in UseRef component; demonstrate differences between state and refs in React.

--- 1-FetchingData/src/pages/react-hooks/use-ref.tsx
+++ 1-FetchingData/src/pages/react-hooks/use-ref.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+/*
+1. Refs are not meant to be used inside return statements
+   (Check by commenting this line ==> setCount((prevCount) => prevCount + 1);)
+   The Ref value won't reflect because it will not trigger a re-render.
 
+2. Refs do not trigger re-renders
+*/
+
+import { useState, useRef } from "react";
 const UseRef = () => {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+
+  const countRef = useRef(count);
+
+  const handleClick = () => {
+    // setCount((prevCount) => prevCount + 1);
+    countRef.current = count + 1;
+    console.log("State: ", count);
+    console.log("Ref: ", countRef.current);
+  };
+  return (
+    <div className="page-container">
+      <h1 className="headline">{countRef.current}</h1>
+      <h1 className="headline">{count}</h1>
+      <button className="button" onClick={handleClick}>
+        Increment
+      </button>
+    </div>
+  );
 };
 
 export default UseRef;
