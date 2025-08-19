71fc5b24954baa1983e4bb7b0a479d84a86d23d2
ramLandmark <ramananda@stylishop.com>
2025-08-20 00:27:30 +0530

    refactor: update README and main.tsx to improve structure and remove StrictMode wrapper

--- React_1/src/main.tsx
+++ React_1/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -20,8 +19,6 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <ReactQueryDevtools initialIsOpen={false} />
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <App />
   </QueryClientProvider>
 );
