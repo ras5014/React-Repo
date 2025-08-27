3818c2f26cf8d09152fad25c7dfc706d154a9ac9
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2025-08-27 14:42:27 +0530

    feat: Enhance SimpleForms component with submission state management and loading indicator

--- React_1/src/pages/react-forms/simple-forms.tsx
+++ React_1/src/pages/react-forms/simple-forms.tsx
@@ -1,11 +1,17 @@
+// If validation and error message showing comes into play then use react-hook-form (For Interviews)
 import { useState } from "react";
 import toast from "react-hot-toast"
 
 export default function SimpleForms() {
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
     const [name, setName] = useState<string>(""); // For multiple form fields this can be an object too
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault(); // Prevents page refresh
         console.log(name);
+        setIsSubmitting(true);
+        // Simulate a form submission
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        setIsSubmitting(false);
         // Check the react-hot-toast implementation in "App.tsx" file
         toast.success(`Form submitted successfully with name: ${name}`);
     }
@@ -15,7 +21,9 @@ export default function SimpleForms() {
             <form onSubmit={(e) => onSubmit(e)}>
                 <div className="flex gap-4">
                     <input type="text" className="input" onChange={(e) => setName(e.target.value)} />
-                    <button className="button" type="submit">Submit</button>
+                    <button className="button" type="submit" disabled={isSubmitting}>
+                        {isSubmitting ? "Submitting..." : "Submit"}
+                    </button>
                 </div>
             </form>
         </div>
